631a8aab6c4b9358e75cd6911c91eb76822c248b
Vikas Tiwari <30732434+ervikasti@users.noreply.github.com>
2024-02-04 00:22:18 +0530

    Fixed: countryCard

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,8 +2,8 @@ import styles from "./Home.module.css";
 
 export default function Home({countries}) {
     return(
-        <div className={styles.countryCard}>{countries.map(country => (
-            <div key={country.cca3}className={styles.card}>
+        <div className={styles.container}>{countries.map(country => (
+            <div key={country.cca3} className={styles.countryCard}>
                 <img src={country.flags.png} alt={`Flag of ${country.name.common}`}/>
                 <h2>{country.name.common}</h2>
             </div>
